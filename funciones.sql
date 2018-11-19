807ea2738effd8e0c11159aacc48e65d1831fd7a
Emilio Basualdo <Pilobas97@gmail.com>
2018-11-19 16:56:47 -0300

    si

--- funciones.sql
+++ funciones.sql
@@ -1,39 +1,42 @@
-/* PARTE A -> CREAMOS LAS TABLAS
+/* PARTE A -> CREAMOS LAS TABLAS */
 
-create table userTP (
-        userId          integer not null,      
+create table usertp (
+        id          integer not null,
         reputation      integer not null,
-        creationDate    timestamp not null,      -- en realidad deberia ser timestamptz porque incluye el timezone pero no me lo acepta
-        displayName     varchar not null,
-        lastAccessDate  timestamp not null,     -- en realidad deberia ser timestamptz porque incluye el timezone pero no me lo acepta
-        websiteUrl      varchar,    
-        location        varchar,
-        aboutMe         varchar not null,      -- hay un tipo que es xml pero no me lo acepta
+        creationdate    timestamp not null,
+        displayname     varchar not null,
+        lastaccessdate  timestamp not null,
+        websiteurl      varchar,
+        location_        varchar,           -- _ por reservada
+        aboutme         varchar,      -- hay un tipo que es xml pero no me lo acepta
         views           integer not null,
-        upVotes         integer not null,
-        downVotes       integer not null,
-        
-        primary key (userId)
+        upvotes         integer not null,
+        downvotes       integer not null,
+        accountid          integer,
+
+        primary key (id)
 );
 
+
 create table badge (
-        id              varchar not null,     -- id del badge                   
-        userId          integer not null,       -- id del usuario  
-        name            varchar not null,     -- nombre del badge
-        date_badge      timestamp not null,     -- en realidad deberia ser timestamptz porque incluye el timezone pero no me lo acepta
-        class           integer not null,
-        
+        id              varchar not null,     -- id del badge
+        userid          integer not null,       -- id del usuario
+        name_            varchar not null,     -- nombre del badge  -- _ por reservada
+        datebadge      timestamp not null,
+        class_           integer not null,     -- _ por reservada
+        tagbased        boolean not null,
+
         primary key (id),
-        foreign key (userId) references userTP
-);*/
+        foreign key (userid) references usertp
+);
 
-/* PARTE B -> INSERTAMOS LOS DATOS -> ESTO ES LO QUE NO ME ANDA
+/* PARTE B -> INSERTAMOS LOS DATOS -> ESTO ES LO QUE NO ME ANDA */
 
-\copy badge from './Badges.tsv' header delimiter ' ';
-\copy userID from './Users.tsv' header delimiter ' ';
-*/
+\copy badge from './Badges.tsv' header delimiter ' ' quote ''';
+\copy userID from './Users.tsv' header delimiter ' ' quote ''';
 
-/* PARTE C -> FUNCIONES
+
+/* PARTE C -> FUNCIONES */
 
 create or replace function reporteBadge (usuarioDesde userTP.userId%type, usuarioHasta userTP.userId%type) return void // tiene que imprimir una tabla, nose si es void
 as
@@ -44,8 +47,3 @@ as
         qtty integer;
 begin
 end;
-
-*/
-        
-
-
